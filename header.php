4f6b52276e3104b1881a4c23afe9c3a5bbe42223
Ben Falconer <ben@falconers.me.uk>
2010-11-19 13:01:49 +0000

    added some more basics

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<title>HTML5 vido player!</title>
+		<link href="main.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
---
+++ view.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'header.php';
+?>
+	<video src="http://zed0.uwcs.co.uk/Misc/UnordinaryWorld.ogg" controls>
+		Your browser does not support html5 video, you should get a better one.
+	</video>
+<?php
+include 'footer.php';
