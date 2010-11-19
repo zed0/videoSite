01aa75f5acbb3633e3b3ce1ba8d1f17ecfb275af
Ben Falconer <ben@falconers.me.uk>
2010-11-19 13:10:24 +0000

    made video fill page

--- view.php
+++ view.php
@@ -2,7 +2,7 @@
 
 include 'header.php';
 ?>
-	<video src="http://zed0.uwcs.co.uk/Misc/UnordinaryWorld.ogg" controls>
+	<video id="mainvideo" src="http://zed0.uwcs.co.uk/Misc/UnordinaryWorld.ogg" controls autobuffer>
 		Your browser does not support html5 video, you should get a better one.
 	</video>
 <?php
